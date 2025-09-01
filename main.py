f00f43f0652fe8d22b7f89a24a5b580c79b0d791
Job Martires <jobmartires@gmail.com>
2025-09-01 20:14:20 +0800

    functioning auto fisher

---
+++ main.py
@@ -0,0 +1,31 @@
+import pyautogui
+import time
+
+pyautogui.useImageNotFoundException()
+
+def main():
+    catch = 1
+    searchRegion = (900, 350, 200, 200)
+
+    while True:
+        try:
+            bite = pyautogui.locateOnScreen("trigger.png", region=searchRegion, confidence=0.9)
+
+            if bite:
+                #reel in fish
+                pyautogui.click(button='right')
+                print('Catch #', catch)
+                catch += 1
+                #recast
+                time.sleep(2)
+                pyautogui.click(button='right')
+            else:
+                continue
+
+        except pyautogui.ImageNotFoundException:
+            continue
+
+main()
+
+
+        
